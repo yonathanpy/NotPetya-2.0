f3cca53b30214a42ccfc0f956fd60c6c7096d097
Yonathan      aka (witwizard,orlserg) <109464957+yonathanpy@users.noreply.github.com>
2026-04-19 18:26:23 +0000

    Create system_inspector.c

---
+++ system_inspector.c
@@ -0,0 +1,9 @@
+#include "system_inspector.h"
+#include <stdio.h>
+
+void inspect_system() {
+    printf("[INSPECTOR] gathering system metadata\n");
+    printf("CPU: detected\n");
+    printf("Memory: mapped\n");
+    printf("Disk: enumerated\n");
+}
